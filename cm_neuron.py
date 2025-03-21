28a956a309ddb8e77d6081326552b045f9aad320
w.wybo <w.wybo@fz-juelich.de>
2025-03-21 10:25:56 +0100

    modify neuron creation to use nestml model

--- cm_neuron.py
+++ cm_neuron.py
@@ -67,6 +67,105 @@ def init_Ca_AdEx(default_params):
         
     return(soma_params, distal_params, other_params)
 
+
+def init_Ca_AdEx_nestml(default_params):
+
+    soma_params = {
+        'C_m': default_params['C_m_s'],                   # [pF] Soma capacitance
+        'g_L': default_params['g_L_s'],                   # [nS] Soma leak conductance
+        'e_L': default_params['e_L_s'],                   # [mV] Soma reversal potential
+        'g_spike': default_params['g_L_s'],          # [nS] Adex conductance
+        'v_thr': default_params['e_Na_Adex'],         # [mV] Adex threshold
+        'delta_thr': default_params['delta_T'],              # [mV] Adex slope factor
+        # 'd_BAP': default_params['d_BAP'],             # delay not yet implemented
+        'refr_period': default_params['t_ref'],
+        'v_reset': default_params['V_reset'],
+        'tau_w': default_params['tau_w'],
+        'a': default_params['a'],
+        'b': default_params['b'],
+        'e_adapt': default_params['e_L_s'],
+        'g_adapt': default_params['g_w'],
+        'w_bap': default_params['w_BAP'],
+    }
+
+    distal_params = {'C_m': default_params['C_m_d'],                 # [pF] Distal capacitance
+                     'g_L': default_params['g_L_d'],                 # [nS] Distal leak conductance
+                     'g_C': default_params['g_C_d'],                 # [nS] Soma-distal coupling conductance
+                     'e_L': default_params['e_L_d'],                 # [mV] Distal reversal potential
+                     'gbar_Ca': default_params['gbar_Ca'],           # [nS] Ca maximal conductance
+                     'gbar_K': default_params['gbar_K_Ca'],       # [nS] K_Ca maximal conductance
+                     'e_K': default_params['e_K'],                   # [mV] K reversal potential
+                     'tau_Ca': default_params['tau_decay_Ca'], # [ms] decay of Ca concentration
+                     'phi_Ca': default_params['phi'],                   # [-] scale factor
+                     'm_half_Ca': default_params['m_half'],             # [mV] m half-value for Ca
+                     'h_half_Ca': default_params['h_half'],             # [mV] h half-value for Ca
+                     'm_slope_Ca': default_params['m_slope'],           # [-] m slope factor for Ca
+                     'h_slope_Ca': default_params['h_slope'],           # [-] h slope factor for Ca
+                     'tau_m_Ca': default_params['tau_m'],               # [ms] m tau decay for Ca
+                     'tau_h_Ca': default_params['tau_h'],               # [ms] h tau decay dor Ca
+                     'tau_m_K': default_params['tau_m_K_Ca'],     # [ms] m tau decay for K_Ca
+                     'c_Ca0': default_params['Ca_0'],                 # [mM] Baseline intracellular Ca conc
+                     'c_Ca_thr': default_params['Ca_th'],               # [mM] Threshold Ca conc for Ca channel opening
+                     'exp_K_Ca': default_params['exp_K_Ca']          # [-] Exponential factor in K_Ca current with Hay dyn
+                     }
+
+    other_params = {'V_th': default_params['V_th']}
+        
+    return(soma_params, distal_params, other_params)
+
+
+def create_nestml_neuron(n = 1, params = {}, multi_comp = True):
+    soma_params, distal_params, other_params = init_Ca_AdEx(params)
+    nest.Install("ca_adex_stdp_module")
+    # initialize the model with two compartments
+    cm = nest.Create("ca_adex_nestml")
+    cm.V_th = other_params['V_th']
+
+    if multi_comp:
+        cm.compartments = [
+            {"parent_idx": -1, "params": soma_params},
+            {"parent_idx":  0, "params": distal_params}
+        ]
+        # receptors
+        receptors = [
+            {"comp_idx": 0, "receptor_type": "syn_2exp", "params": {"tau_r_syn": .173, "tau_d_syn": .227, "e_syn": 0.}}, # ALPHAexc_soma
+            {"comp_idx": 0, "receptor_type": "syn_2exp", "params": {"tau_r_syn": 1.73, "tau_d_syn": 2.27, "e_syn": -85.}}, # ALPHAinh_soma
+            {"comp_idx": 1, "receptor_type": "syn_2exp", "params": {"tau_r_syn": .173, "tau_d_syn": .227, "e_syn": 0.}}, # ALPHAexc_dist
+            {"comp_idx": 1, "receptor_type": "syn_2exp", "params": {"tau_r_syn": 1.73, "tau_d_syn": 2.27, "e_syn": -85.}}, # ALPHAinh_dist
+            {"comp_idx": 1, "receptor_type": "ampa_nmda"}, # AMPA_NMDA_dist
+        ]
+        cm.receptors = receptors
+
+        # receptors get assigned an index which corresponds to the order in which they
+        # are added. For clearer bookkeeping, we explicitly define these indices here.
+        ALPHAexc_soma, ALPHAinh_soma, ALPHAexc_dist, ALPHAinh_dist, AMPA_NMDA_dist = 0, 1, 2, 3, 4
+        # other receptors types are not used, assign something that will generate an error when used
+        GABA_soma, AMPA_soma, GABA_dist, AMPA_dist, REFRACT_soma, ADAPT_soma, BETA_dist, NMDA_soma, AMPA_NMDA_soma, NMDA_dist, = \
+            float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan')
+        
+    else:
+        cm.compartments =[
+            {"parent_idx": -1, "params": soma_params},
+        ]
+        # receptors
+        receptors = [
+            {"comp_idx": 0, "receptor_type": "syn_2exp", "params": {"tau_r_syn": .173, "tau_d_syn": .227, "e_syn": 0.}}, # ALPHAexc_soma
+            {"comp_idx": 0, "receptor_type": "syn_2exp", "params": {"tau_r_syn": 1.73, "tau_d_syn": 2.27, "e_syn": -85.}}, # ALPHAinh_soma
+            {"comp_idx": 0, "receptor_type": "ampa_nmda"}, # AMPA_NMDA_soma
+        ]
+        cm.receptors = receptors
+
+        # receptors get assigned an index which corresponds to the order in which they
+        # are added. For clearer bookkeeping, we explicitly define these indices here.
+        ALPHAexc_soma, ALPHAinh_soma, AMPA_NMDA_soma = 0, 1, 2,
+        # other receptors types are not used, assign something that will generate an error when used
+        GABA_soma, AMPA_soma, GABA_dist, AMPA_dist, REFRACT_soma, ADAPT_soma, BETA_dist, NMDA_soma, ALPHAexc_dist, ALPHAinh_dist, AMPA_NMDA_dist, NMDA_dist, = \
+            float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), float('nan')
+        
+
+    return cm
+
+
 def create_cm_neuron(n = 1, params = {}):
     soma_params, distal_params, other_params = init_Ca_AdEx(params)
     print('soma params: ', soma_params)
@@ -95,23 +194,22 @@ def create_cm_neuron(n = 1, params = {}):
 
     # receptors
     receptors = [
-        {"comp_idx": 0, "receptor_type": "GABA", "params": {"tau_r_GABA": .2, "tau_d_GABA": 2., "e_GABA": -85.}},
-        {"comp_idx": 0, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .2, "tau_d_AMPA": 3., "e_AMPA": 0.}},
-        {"comp_idx": 1, "receptor_type": "GABA", "params": {"tau_r_GABA": .2, "tau_d_GABA": 2., "e_GABA": -85.}},
-        {"comp_idx": 1, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .2, "tau_d_AMPA": 3., "e_AMPA": 0.}},
-        {"comp_idx": 0, "receptor_type": "REFRACT", "params": {"t_ref": t_ref, "g_refract": 100000., "V_reset": V_reset}},
-        {"comp_idx": 0, "receptor_type": "ADAPT", "params": {"tau_w": tau_w, "a": a, "b": b, "e_w": e_w, "g_w": g_w}},
-        {"comp_idx": 1, "receptor_type": "BETA", "params": {"tau_r_BETA": 2., "tau_d_BETA": 5.}},
-        {"comp_idx": 0, "receptor_type": "NMDA"},
-        {"comp_idx": 0, "receptor_type": "AMPA_NMDA"},
-        {"comp_idx": 0, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .173, "tau_d_AMPA": .227, "e_AMPA": 0.}},
-        {"comp_idx": 0, "receptor_type": "AMPA", "params": {"tau_r_AMPA": 1.73, "tau_d_AMPA": 2.27, "e_AMPA": -85.}},
-        {"comp_idx": 1, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .173, "tau_d_AMPA": .227, "e_AMPA": 0.}},
-        {"comp_idx": 1, "receptor_type": "AMPA", "params": {"tau_r_AMPA": 1.73, "tau_d_AMPA": 2.27, "e_AMPA": -85.}},
-        {"comp_idx": 1, "receptor_type": "NMDA"},
-        {"comp_idx": 1, "receptor_type": "AMPA_NMDA"},
+        {"comp_idx": 0, "receptor_type": "GABA", "params": {"tau_r_GABA": .2, "tau_d_GABA": 2., "e_GABA": -85.}}, # GABA_soma,
+        {"comp_idx": 0, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .2, "tau_d_AMPA": 3., "e_AMPA": 0.}}, # AMPA_soma
+        {"comp_idx": 1, "receptor_type": "GABA", "params": {"tau_r_GABA": .2, "tau_d_GABA": 2., "e_GABA": -85.}}, # GABA_dist
+        {"comp_idx": 1, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .2, "tau_d_AMPA": 3., "e_AMPA": 0.}}, # AMPA_dist
+        {"comp_idx": 0, "receptor_type": "REFRACT", "params": {"t_ref": t_ref, "g_refract": 100000., "V_reset": V_reset}}, # REFRACT_soma
+        {"comp_idx": 0, "receptor_type": "ADAPT", "params": {"tau_w": tau_w, "a": a, "b": b, "e_w": e_w, "g_w": g_w}}, #  ADAPT_soma
+        {"comp_idx": 1, "receptor_type": "BETA", "params": {"tau_r_BETA": 2., "tau_d_BETA": 5.}}, # BETA_dist
+        {"comp_idx": 0, "receptor_type": "NMDA"}, # NMDA_soma
+        {"comp_idx": 0, "receptor_type": "AMPA_NMDA"}, # AMPA_NMDA_soma
+        {"comp_idx": 0, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .173, "tau_d_AMPA": .227, "e_AMPA": 0.}}, # ALPHAexc_soma
+        {"comp_idx": 0, "receptor_type": "AMPA", "params": {"tau_r_AMPA": 1.73, "tau_d_AMPA": 2.27, "e_AMPA": -85.}}, # ALPHAinh_soma
+        {"comp_idx": 1, "receptor_type": "AMPA", "params": {"tau_r_AMPA": .173, "tau_d_AMPA": .227, "e_AMPA": 0.}}, # ALPHAexc_dist
+        {"comp_idx": 1, "receptor_type": "AMPA", "params": {"tau_r_AMPA": 1.73, "tau_d_AMPA": 2.27, "e_AMPA": -85.}}, # ALPHAinh_dist
+        {"comp_idx": 1, "receptor_type": "NMDA"}, #  NMDA_dist
+        {"comp_idx": 1, "receptor_type": "AMPA_NMDA"}, # AMPA_NMDA_dist
     ]
-    
     cm.receptors = receptors
 
     # receptors get assigned an index which corresponds to the order in which they
